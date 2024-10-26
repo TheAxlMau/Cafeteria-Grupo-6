2755b3b0db0cc13cbf29dae59f5bc9bd724eb72b
Axl <theaxllay@gmail.com>
2024-10-26 02:40:31 -0500

    prueba 1 con un label

--- Cafeteria/menucafeteria.Designer.cs
+++ Cafeteria/menucafeteria.Designer.cs
@@ -30,21 +30,24 @@
         {
             this.Panelmenu = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.btnProductos = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Panelmenu.SuspendLayout();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // Panelmenu
             // 
             this.Panelmenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Panelmenu.Controls.Add(this.label1);
             this.Panelmenu.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Panelmenu.Location = new System.Drawing.Point(173, 51);
             this.Panelmenu.Name = "Panelmenu";
@@ -68,6 +71,26 @@
             this.panel4.Size = new System.Drawing.Size(173, 465);
             this.panel4.TabIndex = 4;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inventario";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(74, 315);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Nota ingreso";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(8, 100);
@@ -87,26 +110,6 @@
             this.button6.Text = "Proveedores";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(74, 315);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
-            this.button7.TabIndex = 0;
-            this.button7.Text = "Nota ingreso";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(74, 364);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Inventario";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button5
             // 
@@ -157,6 +160,16 @@
             this.panel1.Size = new System.Drawing.Size(997, 51);
             this.panel1.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(184, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prueba1";
+            // 
             // menucafeteria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -167,6 +180,8 @@
             this.Controls.Add(this.panel1);
             this.Name = "menucafeteria";
             this.Text = "Form1";
+            this.Panelmenu.ResumeLayout(false);
+            this.Panelmenu.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -185,6 +200,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button btnProductos;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
