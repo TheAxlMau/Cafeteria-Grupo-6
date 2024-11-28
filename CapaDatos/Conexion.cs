0949d24b4da94b485127057c98606c8aa272e38b
Gerardo Erick Plasencia Torres <N00340097@upn.pe>
2024-11-28 01:16:19 -0500

    Se agregara Conexion

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        #region Singleton
+        private static readonly Conexion _instancia = new Conexion();
+        public static Conexion Instancia
+        {
+            get { return Conexion._instancia; }
+        }
+        #endregion Singleton
+
+        #region Metodos
+        public SqlConnection conectar()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=localhost; Initial Catalog = CafeteriaGosqueDB;" +
+                                "Integrated Security=true";
+            return cn;
+        }
+        #endregion Metodos
+    }
+}
